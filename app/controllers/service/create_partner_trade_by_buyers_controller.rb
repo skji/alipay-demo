f93d08a647d0849e9e250ee5ac62b01da9589737
Rei <chloerei@gmail.com>
2015-03-24 17:02:51 +0800

    create_partner_trade_by_buyers create action

--- app/controllers/service/create_partner_trade_by_buyers_controller.rb
+++ app/controllers/service/create_partner_trade_by_buyers_controller.rb
@@ -1,4 +1,25 @@
 class Service::CreatePartnerTradeByBuyersController < ApplicationController
   def new
   end
+
+  def create
+    order = Order.create
+
+    redirect_to Alipay::Service.create_partner_trade_by_buyer_url(
+      out_trade_no: order.number,
+      subject: "AlipayDemo #{order.number}",
+      price: order.price,
+      quantity: 1,
+      discount: 0,
+      return_url: return_service_create_partner_trade_by_buyer_url,
+      notify_url: notify_service_create_partner_trade_by_buyer_url,
+      logistics_type: 'DIRECT',
+      logistics_fee: 0,
+      logistics_payment: 'SELLER_PAY',
+      receive_name: 'none',
+      receive_address: 'none',
+      receive_zip: '100000',
+      receive_mobile: '100000000000'
+    )
+  end
 end
