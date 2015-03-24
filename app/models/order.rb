4eb5f6a681f48b53b450a7011cb913b5ec5b5e5a
Rei <chloerei@gmail.com>
2015-03-24 16:22:00 +0800

    Fixed order price to 0.01

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+  def price
+    0.01
+  end
 end
--- db/migrate/20150324030744_create_orders.rb
+++ db/migrate/20150324030744_create_orders.rb
@@ -2,7 +2,6 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.string :number, null: false
-      t.decimal :price, precision: 10, scale: 2, null: false
 
       t.timestamps null: false
     end
