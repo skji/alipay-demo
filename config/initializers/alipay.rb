d940e4406054efd99c0b300c013579972e6f8d9b
Rei <chloerei@gmail.com>
2015-03-30 14:11:19 +0800

    Add sign type ENV

--- config/initializers/alipay.rb
+++ config/initializers/alipay.rb
@@ -1,3 +1,4 @@
 Alipay.pid = ENV['ALIPAY_PID']
 Alipay.key = ENV['ALIPAY_KEY']
 Alipay.seller_email = ENV['ALIPAY_SELLER_EMAIL']
+Alipay.sign_type = ENV['ALIPAY_SIGN_TYPE'] if ENV['ALIPAY_SIGN_TYPE'].present?
